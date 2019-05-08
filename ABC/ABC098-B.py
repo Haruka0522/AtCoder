c501d0b1a391fa95e3d65d76a028d164fcb379bc
Haruka0522 <harukapc1@gmail.com>
2019-05-08 09:56:36 +0900

    ABC098-Bを解いた

--- ABC/ABC098-B.py
+++ ABC/ABC098-B.py
@@ -1,3 +1,13 @@
-import collections
 N = int(input())
-S = list(input())
+S = input()
+ans_list = []
+abc_list = [chr(i) for i in range(97,97+26)]
+for i in range(1,N):
+    ans = 0
+    S1 = list(S[:i])
+    S2 = list(S[i:])
+    for j in abc_list:
+        if(j in S1 and j in S2):
+            ans += 1
+    ans_list.append(ans)
+print(max(ans_list))
