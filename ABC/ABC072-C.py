621ee8a5e3d5eba42201be1082db9cb4d6a7b000
Haruka0522 <harukapc1@gmail.com>
2019-05-19 14:30:56 +0900

    ABC072-C解説AC

--- ABC/ABC072-C.py
+++ ABC/ABC072-C.py
@@ -12,4 +12,13 @@ for X in range(min(A),max(A)+1):
 print(max(ans))
 '''
 
-
+#解説を見て
+import collections
+N = int(input())
+A = list(map(int,input().split()))
+list_A = []
+for i in  range(N):
+    list_A.append(A[i])
+    list_A.append(A[i]+1)
+    list_A.append(A[i]-1)
+print(collections.Counter(list_A).most_common()[0][1])
