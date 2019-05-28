f68bde07febccd7cf0521f34fe45b2fc119646f2
Haruka0522 <harukapc1@gmail.com>
2019-05-28 13:19:41 +0900

    ABC100-B自力WA

--- ABC/ABC100-B.py
+++ ABC/ABC100-B.py
@@ -1,3 +1,4 @@
+'''WA code
 D, N = map(int, input().split())
 ans = []
 count = 0
@@ -8,3 +9,14 @@ while (True):
     except:
         ans.append((100 ** D) * count)
     count += 1
+'''
+
+'''WA code
+#N == 100のときの処理がない
+D,N = map(int,input().split())
+if D==0:
+    print(N)
+elif D==1:
+    print(N*100)
+else:
+    print(N*10000)
