6ed3b05a2fbe22a596db7f20a44648e93f0223e7
Haruka0522 <harukapc1@gmail.com>
2019-05-28 15:49:32 +0900

    for文のrangeを修正しAC

--- ABC/ABC085-C.py
+++ ABC/ABC085-C.py
@@ -1,6 +1,6 @@
 N,Y = map(int,input().split())
-for yukichi in range(int(Y/10000)+1):
-        for ichiyo in range(int(Y/5000)+1):
+for yukichi in range(N+1):
+        for ichiyo in range(N+1):
             hideyo = N-yukichi-ichiyo
             if(10000*yukichi+5000*ichiyo+1000*hideyo == Y and hideyo >= 0):
                 print(yukichi,ichiyo,hideyo)
