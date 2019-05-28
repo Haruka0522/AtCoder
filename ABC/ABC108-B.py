be00d37251cc0f8c5f334ea15588684540339a7f
Haruka0522 <harukapc1@gmail.com>
2019-05-28 13:04:50 +0900

    ABC108-Bを解説AC

--- ABC/ABC108-B.py
+++ ABC/ABC108-B.py
@@ -1,9 +1,4 @@
-import math
-x1, y1, x2, y2 = map(int, input().split())
-ippen = math.sqrt((y2 - y1) ** 2 + (x2 - x1)**2)
-taikakusen = math.sqrt(ippen ** 2)
-x3 = taikakusen ** 2 - x1 ** 2
-y3 = taikakusen ** 2 - y1 ** 2
-x4 = taikakusen ** 2 - x2 ** 2
-y4 = taikakusen ** 2 - y2 ** 2
-print(x3, y3, x4, y4)
+x1,y1,x2,y2 = map(int,input().split())
+a = x2 - x1
+b = y2 - y1
+print(x2-b,y2+a,x1-b,y1+a)
