cb7ff5bd32603c43e380fc7aba01c7300531a214
Haruka0522 <harukapc1@gmail.com>
2019-06-07 09:42:33 +0900

    diverta2019-BをAC

--- Others/diverta2019-B.py
+++ Others/diverta2019-B.py
@@ -1,11 +1,7 @@
 R,G,B,N = map(int,input().split())
 ans = 0
-RGB = sorted([R,G,B],reverse=True)
-R,G,B = RGB[0],RGB[1],RGB[2]
-for r in range(int(N/R)+1):
-    for g in range(int((N-R*r)/G)+1):
-        for b in range(int((N-(R*r+G*g))/B)+1):
-            if(R*r+G*g+B*b == N):
-                ans += 1
+for r in range(N//R+1):
+    for g in range((N-R*r)//G+1):
+        if (N-R*r-G*g)%B == 0:
+            ans += 1
 print(ans)
-
