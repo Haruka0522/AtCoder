22e0f97a527d0cf84982653e015271dab9b46ccb
Haruka0522 <harukapc1@gmail.com>
2019-07-07 23:14:35 +0900

    ABC133-Cを解説AC

--- ABC/ABC133-C.py
+++ ABC/ABC133-C.py
@@ -1,6 +1,19 @@
+'''大会中コード
 L,R = map(int,input().split())
 ans = 2019
 for j in range(R+1):
     for i in range(L,j):
         ans = min(ans,(i*j)%2019)
+print(ans)
+'''
+
+#解説を見て
+L,R = map(int,input().split())
+ans = 2018
+for i in range(L,R):
+    for j in range(i+1,R+1):
+        ans = min(ans,(i*j)%2019)
+        if ans == 0:
+            print(0)
+            exit()
 print(ans)
