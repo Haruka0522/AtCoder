f7effdfc5d837aa6ace204726801ed63de1576c0
Haruka0522 <harukapc1@gmail.com>
2020-01-14 22:16:08 +0900

    やろうとしたけど時間切れで断念（途中）

--- ABC/ABC128-C.py
+++ ABC/ABC128-C.py
@@ -1,1 +1,10 @@
 N,M = map(int,input().split())
+bin_list = []
+ans = 0
+for i in range((N)**2):
+    bin_list = [0] * N
+    for j in range(N):
+        if i>>j & 1:
+            bin_list[j] = 1
+
+    
