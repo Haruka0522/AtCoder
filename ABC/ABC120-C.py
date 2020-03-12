6da58207f9d9c58cb61c47f8ca0146098dfec7f6
Haruka0522 <harukapc1@gmail.com>
2020-03-12 10:36:02 +0900

    ABC120-Cを解説AC

--- ABC/ABC120-C.py
+++ ABC/ABC120-C.py
@@ -1,12 +1,4 @@
 S = list(input())
-original = len(S)
-flag = True
-while flag:
-    flag = False
-    i = 0
-    while i+1 < len(S):
-        if S[i] != S[i+1]:
-            flag = True
-            del S[i:i+2]
-        i += 1
-print(original-len(S))
+cnt_1 = S.count("1")
+cnt_0 = S.count("0")
+print(2*min(cnt_1,cnt_0))
