d79f4166dc95e6c0b97b02f5d264d595f0f72eb4
Haruka0522 <harukapc1@gmail.com>
2020-03-31 21:09:40 +0900

    ABC017-Bを自力AC

--- ABC/ABC017-B.py
+++ ABC/ABC017-B.py
@@ -1,38 +1,23 @@
-S = list(input())
-flag = 0
-for i in range(2):
-    if (len(S) == 0):
-        print("YES")
-        exit()
-
-    if (flag == 0):
-        if (S[-2::] == ["c", "h"]):
-            del S[-2::]
-            flag = 1
-        else:
-            flag = 2
-
-        if (S[-1] in ["o", "k", "u"]):
-            del S[-1]
-            flag = 1
-        else:
-            flag = 2
-
-    elif (flag == 1):
-        if (S[-2::] == ["c", "h"]):
-            print("YES")
-        else:
-            flag = 2
-
-        if (S[-1] in ["o", "k", "u"]):
-            print("YES")
-        else:
-            flag = 2
-
-        if (len(S) == 0):
-            print("YES")
-        else:
-            flag = 2
-
-if (flag == 2):
+X = input()
+Flag = True
+while Flag:
+    Flag = False
+    sue = X[-1]
+    if X[-2:] == "ch":
+        X = X[:-2]
+        Flag = True
+    elif sue == "o":
+        X = X[:-1]
+        Flag = True
+    elif sue == "k":
+        X = X[:-1]
+        Flag = True
+    elif sue == "u":
+        X = X[:-1]
+        Flag = True
+    if len(X) <= 1:
+        break
+if X == "":
+    print("YES")
+else:
     print("NO")
