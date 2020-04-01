7e62f0c33a3e99d0e1a042e1bb8ad4fe0496b2c6
Haruka0522 <harukapc1@gmail.com>
2020-04-01 19:40:40 +0900

    ABC022-Bを自力AC

--- ABC/ABC022-B.py
+++ ABC/ABC022-B.py
@@ -1,9 +1,10 @@
 N = int(input())
-checked = []
+checked = [0]*10**5
 ans = 0
 for i in range(N):
     A = int(input())
-    if A in checked:
+    if checked[A-1] == 1:
         ans += 1
-    checked.append(A)
+    else:
+        checked[A-1] = 1
 print(ans)
