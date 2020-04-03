d6f93b92ab9dfc08bd7665c19160b720b74a53b2
Haruka0522 <harukapc1@gmail.com>
2020-04-03 21:56:44 +0900

    ABC132-Cを自力AC

--- ABC/ABC132-C.py
+++ ABC/ABC132-C.py
@@ -1,11 +1,4 @@
 N = int(input())
-D = list(map(int,input().split()))
-ans = 0
-for K in range(min(D),max(D)):
-    ABC = 0
-    for d in D:
-        if d < K:
-            ABC += 1
-    if ABC == N-ABC:
-        ans += 1
-print(ans)
+d_list = list(map(int,input().split()))
+d_list.sort()
+print(d_list[N//2]-d_list[N//2-1])
