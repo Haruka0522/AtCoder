fcab0b8d60a2cf241768780d5fc3b76a83a5c736
Haruka0522 <harukapc1@gmail.com>
2020-04-03 21:58:44 +0900

    ABC146-Cを自力AC

--- ABC/ABC146-C.py
+++ ABC/ABC146-C.py
@@ -1,8 +1,23 @@
-#WAコード
+import bisect
 A,B,X = map(int,input().split())
-ans = 0
-for i in reversed(range(111111112)):
-    if A*i + B*(len(str(i))) < X:
-        ans = i
+low = 0
+high = 10**9
+while low <= high:
+    mid = (low + high) // 2
+    kingaku = A * mid + B * len(str(mid))
+    mae = (low,high)
+    if kingaku < X:
+        low = mid - 1
+    elif kingaku > X:
+        high = mid + 1
+    else:
+        print(mid)
+        exit()
+    if mae == (low,high):
         break
-print(ans)
+#print(low,high)
+for i in range(high,low-1,-1):
+    if A * i + B * len(str(i)) <= X:
+        print(i)
+        exit()
+print(0)
