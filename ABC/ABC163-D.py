a240e6b9f8d6577134d8a42f65053b42020f6765
Haruka0522 <harukapc1@gmail.com>
2020-04-19 23:18:14 +0900

    ABC163-Dを他の人の解答を見てAC

--- ABC/ABC163-D.py
+++ ABC/ABC163-D.py
@@ -1,14 +1,6 @@
-from scipy.special import comb
-from math import factorial
 N,K = map(int,input().split())
 ans = 0
 mod = 10**9+7
-N += 1
-a = factorial(N)//factorial(N-K)
-j = K
-for i in range(K,N+1):
-    print(a,j)
-    ans += (a//j) % mod
-    a *= (N-i)
-    j *= (i+1)
-print(ans)
+for k in range(K,N+2):
+    ans += (k*(N-k+1)+1)
+print(ans % mod)
