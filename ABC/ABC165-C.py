b3956530d046c31d7af81e8f5e031963c01fef26
Haruka0522 <harukapc1@gmail.com>
2020-05-03 10:29:19 +0900

    ABC165-CをAC

--- ABC/ABC165-C.py
+++ ABC/ABC165-C.py
@@ -1,17 +1,11 @@
+from itertools import combinations_with_replacement
 N,M,Q = map(int,input().split())
 ans_li = []
 abcd = [tuple(map(int,input().split())) for _ in range(Q)]
-max_lim = sum([i[3] for i in abcd])
-for n in range(10**N):
+for A in combinations_with_replacement(range(1,M+1),N):
     ans = 0
-    if len(str(n)) != N:
-        continue
-    A = list(str(n))
     for a,b,c,d in abcd:
-        if int(A[b-1]) - int(A[a-1]) == c:
+        if A[b-1] - A[a-1] == c:
             ans += d
     ans_li.append(ans)
-    if ans >= max_lim:
-        print(max_lim)
-        exit()
 print(max(ans_li))
