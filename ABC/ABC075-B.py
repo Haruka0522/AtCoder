e006f744e84a79250f61bbe83ac45b7cf573e142
Haruka0522 <harukapc1@gmail.com>
2020-05-05 16:32:08 +0900

    ABC075-BをRE

---
+++ ABC/ABC075-B.py
@@ -0,0 +1,21 @@
+import numpy as np
+H,W = map(int,input().split())
+table = [["."]*(W+2),]
+for _ in range(H):
+    table.append([".",]+list(input())+[".",])
+table.append(["."]*(W+2))
+table = np.array(table)
+ans = []
+#print(table)
+for h in range(H):
+    for w in range(W):
+        mawari = table[h:h+3,w:w+3]
+        #print(mawari)
+        if mawari[1,1] == ".":
+            ans.append(np.count_nonzero(mawari=="#"))
+        else:
+            ans.append("#")
+ans = np.array(ans).reshape(H,W)
+#print(ans)
+for i in ans:
+    print("".join(i))
